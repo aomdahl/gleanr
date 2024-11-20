830e8e7d67c81722e11a6e2b36e52fbf5e241eeb
aomdahl <aomdahl1@gmail.com>
2024-11-20 10:30:30 -0500

    cleaning up

--- src/formatting_functs.R
+++
@@ -1,8 +0,0 @@
-
-standardizeColNames <- function(cin)
-{
-    #tidyr doesn't like some names, so just trying to make sure things are consistent....
-    gsub(x = cin, pattern = "\\.", replacement = "-") %>% 
-          gsub(x = ., pattern = "_-", replacement = "_") %>%
-          gsub(x = ., pattern = "-$", replacement = "") %>% gsub( x= ., pattern = "^X(\\w)", replacement = "\\1")
-}
